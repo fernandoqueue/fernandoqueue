078fa853fc2bff0dc1b776fb4c0bf34925d8847f
Fernando Quintero <fernando_q89@yahoo.com>
2018-05-16 16:45:14 -0700

    testiq

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
-function changetext(){
-	var x = document.getElementById("bodies");
-	if(x.innerHTML==="dd"){
-	x.innerHTML="Good Bye World;
-	}
-	else{
-	x.innerHTML ="Hello World";
-	}		
+function changetext(x){
+	switch(x){
+	case 1:	
+	document.getElementById("bodies1").style.display = "block";
+	document.getElementById("bodies2").style.display = "none";
+	document.getElementById("bodies3").style.display = "none";
+	document.getElementById("bodies4").style.display = "none";
+	break;
+	case 2:	
+	document.getElementById("bodies1").style.display = "none";
+	document.getElementById("bodies2").style.display = "block";
+	document.getElementById("bodies3").style.display = "none";
+	document.getElementById("bodies4").style.display = "none";
+	break;
+	case 3:	
+	document.getElementById("bodies1").style.display = "none";
+	document.getElementById("bodies2").style.display = "none";
+	document.getElementById("bodies3").style.display = "block";
+	document.getElementById("bodies4").style.display = "none";
+	break;
+	case 4:	
+	document.getElementById("bodies1").style.display = "none";
+	document.getElementById("bodies2").style.display = "none";
+	document.getElementById("bodies3").style.display = "none";
+	document.getElementById("bodies4").style.display = "block";
+	break;
+
+
+}
 }
