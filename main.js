89f57be8f2ae4858f51812515369ed6ad3c1974f
Fernando Quintero <38089249+fernandoqueue@users.noreply.github.com>
2018-05-16 13:26:45 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function changetext(){
 	var x = document.getElementById("bodies");
 	if(x.innerHTML==="Hello World"){
-	x.innerHTML="Good Bye World;
+	x.innerHTML="Good Bye World";
 	}
 	else{
 	x.innerHTML ="Hello World";
